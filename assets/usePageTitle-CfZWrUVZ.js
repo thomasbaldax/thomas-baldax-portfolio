91ad50fec69964f1b63e13d2e0eb9a081bbc38c9
thomasbaldax <thomas_baldax@libero.it>
2025-07-24 18:51:42 +0200

    Updates

--- assets/usePageTitle-CfZWrUVZ.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-FI1VtGWK.js";const t="Thomas Baldax",a=e=>{o.useEffect(()=>{e?document.title=`${e} | ${t}`:document.title=t},[e])};export{a as u};
---
+++ assets/usePageTitle-Fl8Yowar.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-Dv6JITvr.js";const t="Thomas Baldax",a=e=>{o.useEffect(()=>{e?document.title=`${e} | ${t}`:document.title=t},[e])};export{a as u};
